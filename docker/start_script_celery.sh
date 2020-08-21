3280861532cced9db827c6677e7814e55394dbda
Sandro Loch <es.loch@gmail.com>
2020-08-20 22:02:28 -0300

    Configure downloaderapp to start on celery

--- docker/start_script_celery.sh
+++ docker/start_script_celery.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 exec celery worker -A crawlclima.fetchapp -l info --concurrency=4 &
-exec python downloader_app/ee_start.py &
 exec celery worker -A downloader_app.celeryapp -l info &
 cron && tail -f /var/log/cron.log
 
