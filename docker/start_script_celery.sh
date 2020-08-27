615136baa7f27bad60246dec743027bdc9c04be2
Sandro Loch <es.loch@gmail.com>
2020-08-27 11:18:32 -0300

    Add make deploy to the CI workflow

--- docker/start_script_celery.sh
+++ docker/start_script_celery.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+python setup.py develop
 exec celery worker -A crawlclima.fetchapp -l info --concurrency=4 &
 exec celery worker -A downloader_app.celeryapp -l info &
 cron && tail -f /var/log/cron.log
